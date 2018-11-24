d345cc807db2d474b084ed4c65f9903db71281a5
System Administrator <root@DavidArMacBook3.hsd1.mn.comcast.net>
2018-11-24 14:30:31 -0600

    Add custom plugin to the election page

--- page-elections-template.php
+++ page-elections-template.php
@@ -15,22 +15,31 @@
 <?php
 
 	/*
-		FILTER_SANITIZE_NUMBER_FLOAT
- 	   	FILTER_SANITIZE_SPECIAL_CHARS
-    	FILTER_SANITIZE_STRING
-    	FILTER_SANITIZE_URL
-    	FILTER_SANITIZE_ENCODED
+	Sanitize Input
+	The sanitize_*() class of helper functions are super nice for us, as they ensure we're ending up with safe data and require minimal effort on our part:
 
- 	
-		$var=(filter_var($var, FILTER_SANITIZE_NUMBER_INT));
-		$var=(filter_var($var,  FILTER_SANITIZE_EMAIL));
-		$var=(filter_var($var, FILTER_SANITIZE_STRING));
+	    sanitize_email()
+	    sanitize_file_name()
+	    sanitize_html_class()
+	    sanitize_key()
+	    sanitize_meta()
+	    sanitize_mime_type()
+	    sanitize_option()
+	    sanitize_sql_orderby()
+	    sanitize_text_field()
+	    sanitize_textarea_field()
+	    sanitize_title()
+	    sanitize_title_for_query()
+	    sanitize_title_with_dashes()
+	    sanitize_user()
 
-esc_html() – Use this function anytime an HTML element encloses a section of data being displayed.
-esc_url() – Use this function on all URLs, including those in the src and href attributes of an HTML element.
-esc_js() – Use this function for inline Javascript.
-esc_attr() – Use this function on everything else that’s printed into an HTML element’s attribute.
-esc_textarea() – encodes text for use inside a textarea element.
+	Escaping Output
+	For security on the other end of the spectrum, we have escaping. To escape is to take the data you may already have and help secure it prior to rendering it for the end user.
+		esc_html() – Use this function anytime an HTML element encloses a section of data being displayed.
+		esc_url() – Use this function on all URLs, including those in the src and href attributes of an HTML element.
+		esc_js() – Use this function for inline Javascript.
+		esc_attr() – Use this function on everything else that’s printed into an HTML element’s attribute.
+		esc_textarea() – encodes text for use inside a textarea element.
 
 	 */
 ?>
@@ -40,62 +49,33 @@ esc_textarea() – encodes text for use inside a textarea element.
 <div class="row"> <!-- Main Row -->
 	<div class="col-12 col-sm-8">	<!-- Content Column -->
 		<div class="row"> <!-- Races Row -->
-<?php 
+			<?php
 
-global $wpdb;
-$params = array();
+			 if(function_exists('the_widget')) { // If the_widget is supported
 
-if (! get_query_var('race')): // Display Race Widget
+			 	$instance = array(
+			 				'title' => '',
+			 				'banner' => '1140StateExecutiveRacesBanner.jpg',
+							'races'	=> 'RACE004,RACE005,RACE006',
+							'noCandidates' => '',
+							'linkURL' => '',
+							'linkLabel' => '',   
+							'active' => '1');
+			 	
+			 	$args = array(
+							'description'   => '',
+    						'class'         => '',
+							'before_widget' => '<li id="dagoMainElectionPageWidget" class="widget dagoMEP">',
+							'after_widget'  => '</li>',
+							'before_title'  => '<h2 class="widgettitle">',
+							'after_title'   => '</h2>' );
 
-	$params[1]  = filter_var(1, FILTER_SANITIZE_NUMBER_INT) ;
-	$query = "SELECT raceUniqueID, title1, precintsPercentage FROM da_election_races WHERE raceActive = %s";
-	$races = $wpdb->get_results( $wpdb->prepare( $query, $params) ) or die( $wpdb->last_error );
-
-	foreach ($races as $race) {
-		$params = array();
-		$params[1]  = filter_var($race->raceUniqueID, FILTER_SANITIZE_STRING) ;
-		$query = "SELECT * FROM da_election_candidates WHERE raceUniqueID = %s ORDER BY numberVotes Desc";
-		$candidates = $wpdb->get_results( $wpdb->prepare( $query, $params) ) or die( $wpdb->last_error );
-
-?>
-			<div class="col-12 col-sm-6 col-md-4" style="margin-top:5px;">
-				<div class="row">
-					<div class="col-12"><h4><?php echo esc_html($race->title1); ?></h4></div>
-					<div class="col-12"><h5><?php echo 'Reporting: ', esc_html($race->precintsPercentage), '%'; ?></h5></div>
-					<?php foreach ($candidates as $cand) { ?>
-						<div class="col-1"><h6><?php if ($cand->winner) echo 'X'; ?></h6></div>
-						<div class="col-8">
-							<h6><?php echo esc_html($cand->lastName), '(', esc_html($cand->affiliation), ')'; ?></h6>
-						</div>
-						<div class="col-2">
-							<h6><?php echo esc_html($cand->percentageVotes), '%'; ?></h6>
-						</div>
-					<?php } ?>
-					<?php $qparams = array( 'race' => $race->raceUniqueID ); ?>
-					<div class="col-11">
-						<a href="<?php echo add_query_arg($qparams, '/election/'); ?>"><small>Full Results</small></a>
-					</div>
-				</div>
-			</div>
-		<?php } ?>
-<?php 
-
-else: // Display Individual Page
+		 		the_widget( 'DAGO_Election_Results_Widget', $instance, $args); 
+		 	 
+		 	 }
+		 	 	
+		 	?>  
 	
-	$params[1]  = filter_var(get_query_var('race'), FILTER_SANITIZE_STRING) ;
-	$query = "SELECT raceUniqueID, title1, title2, lastUpdated, precintsPercentage FROM da_election_races WHERE raceUniqueID = %s";
-	$race = $wpdb->get_row( $wpdb->prepare( $query, $params) ) or die( $wpdb->last_error );
-
-?>
-
-		<div class="col-12" style="margin-top: 15px;"><h1><?php echo esc_html($race->title1); ?></h1></div>
-		<div class="col-12"><h3><?php echo esc_html($race->title2); ?></h3></div>
-		<div class="col-12"><h5><?php echo 'Last Updated: ', esc_html($race->lastUpdated); ?></h5></div>
-		<div class="col-11">
-			<a href="/election/"><small>Back to Main Election Page</small></a>
-		</div>
-
-<?php endif; ?>
 		</div> <!-- Races Row -->
 	</div> <!-- End Content Column -->
 	<div class="col-12 col-sm-4">	<!-- Sidebar Column -->
